97b0410f40daf6165239bdd26ca5f256a5be867e
masatokai1212 <ishome.kai@gmail.com>
2024-12-20 17:57:50 +0900

    Buttonコンポーネントから不要なコメントを削除しました

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-// src/components/ui/button.jsx
-import React from 'react';
-
 export const Button = (props) => {
   return <button {...props}>{props.children}</button>;
 };
