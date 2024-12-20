f206329824115e18dd7f3ce4702b3ac3386c8629
masatokai1212 <ishome.kai@gmail.com>
2024-12-20 17:56:54 +0900

    Buttonコンポーネントを簡素化し、クラス名の適用を削除しました

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-const Button = ({ children, className, ...props }) => {
-  return (
-    <button className={`px-4 py-2 rounded ${className}`} {...props}>
-      {children}
-    </button>
-  );
+// src/components/ui/button.jsx
+import React from 'react';
+
+export const Button = (props) => {
+  return <button {...props}>{props.children}</button>;
 };
 
 export default Button;
